89451989c6c11f8cea6f899a3c230e248203937b
Rahmat A Trikada <ralrafi12@gmail.com>
2021-09-13 14:34:50 +0700

    2 Moving Character & Using Physic

---
+++ Assets/Scripts/CharacterMoveController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    [Header("Movement")] 
+    public float moveAccel;
+    public float maxSpeed;
+
+    private Rigidbody2D rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 vectorVelocity = rig.velocity;
+        vectorVelocity.x = Mathf.Clamp(vectorVelocity.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+
+        rig.velocity = vectorVelocity;
+    }
+}
