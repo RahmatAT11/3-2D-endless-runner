083d5fb53b79e8066285439f3cba6b3a182c14c0
Rahmat A Trikada <ralrafi12@gmail.com>
2021-09-13 15:24:50 +0700

    2 Using Raycast & Layers

--- Assets/Scripts/CharacterMoveController.cs
+++ Assets/Scripts/CharacterMoveController.cs
@@ -14,8 +14,12 @@ public class CharacterMoveController : MonoBehaviour
     [Header("Jump")] 
     public float jumpAccel;
 
-    private bool isJumping;
-    private bool isOnGround;
+    public bool isJumping;
+    public bool isOnGround;
+
+    [Header("Ground Raycast")] 
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
 
     private void Start()
     {
@@ -36,9 +40,38 @@ public class CharacterMoveController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // raycast ground
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down,
+            groundRaycastDistance, groundLayerMask);
+        if (hit)
+        {
+            if (!isOnGround && rig.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+        
+        // kalkulasi velocity vector
         Vector2 vectorVelocity = rig.velocity;
-        vectorVelocity.x = Mathf.Clamp(vectorVelocity.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
+
+        if (isJumping)
+        {
+            vectorVelocity.y += jumpAccel;
+            isJumping = false;
+        }
+        
+        //vectorVelocity.x = Mathf.Clamp(vectorVelocity.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
 
         rig.velocity = vectorVelocity;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down * groundRaycastDistance), 
+            Color.white);
     }
 }
